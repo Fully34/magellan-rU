71abf95477d0e3c1bf501671ad662051e7f77266
Fully34 <cfullinw34@gmail.com>
2015-07-30 17:27:22 -0600

    tidied up

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
 var voyage = require('./models/voyage.js');
 
-//============================== The app ==============================//
+//============================== config ==============================//
         
 var app = express();
 app.set('view engine', 'jade');
@@ -14,10 +14,19 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
+//============================== routing ==============================//
+
+//- .index method redirects to the first location on the voyage
 app.get('/', indexController.index)
+
+//- .voyage method takes the locationName from the req.params object
 app.get('/voyage/:locationName', indexController.voyage);
+
+//- send json information from query string
 app.get('/next', indexController.next)
 
+//============================== server setup ==============================//
+        
 var server = app.listen(5025, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,25 @@
+//- requiring the data structure 
 var voyage = require('../models/voyage.js');
 
 var indexController = {
 
     index : function(req, res) {
 
+        // redirect from index page, so user doesn't have to type in a more complex url
         res.redirect('/voyage/' + voyage.first.name);
     },
 
 	voyage: function(req, res) {
 
-        console.log(voyage);
-
+        //- looking at the location from locations sub-object with the same name as locationName in the url
         var location = voyage.locations[req.params.locationName];
 
-
+        //- handle when the locationName is not a key in the locations sub-object 
         if (!location) {
 
             res.render('magellandidnotgothere', {locationName : req.params.locationName});
 
+        //-render the voyage jade file with a reference to the discrete location object specified in the above location variable
         } else { 
 
             res.render('voyage', location);
@@ -25,8 +27,12 @@ var indexController = {
     },
 
     next: function(req, res) {
+
+        //- find the location object that matches the query string from the req object
+            //- toLowerCase() takes care of capitalization problems
         var location = voyage.locations[req.query.location.toLowerCase()];
 
+        //- if the location exists, construct and send the json object to the screen
         if (location) {
             var nextLocName = location.next ? location.next.name : null;
 
@@ -35,6 +41,7 @@ var indexController = {
                 nextLocation: nextLocName
             });
 
+        //- otherwise, send an empty object
         } else {
 
             res.send({});
